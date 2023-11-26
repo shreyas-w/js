c124c2443e81e1722cddc82466763db6f6106fae
shreyas-w <151855799+shreyas-w@users.noreply.github.com>
2023-11-26 16:08:41 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+/*
+data types 
+
+1 number 
+2 bigint
+3 string    ""
+4 boolean    true/false
+5 null  =>standalone value
+6 Undefined
+7 symbol
+8 object
+
+
+*/
+
+
+console.log(typeof null);    // type of null is object
+console.log(typeof undefined);  //type of undefined is undefined
